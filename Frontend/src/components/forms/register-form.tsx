94edd937c8604faefe77f261ded7272834ce6237
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-23 09:24:55 -0300

    feat(auth): complete register form layout and styling

--- Frontend/src/components/forms/register-form.tsx
+++ Frontend/src/components/forms/register-form.tsx
@@ -9,24 +9,63 @@ import {
 } from "@/components/ui/card"
 import { Label } from '@radix-ui/react-label'
 import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 export function RegisterForm() {
   return (
     <div>
-        <Card>
+        <Card className="bg-app-card border-app-border w-full max-w-md mx-auto">
             <CardHeader>
-                <CardTitle>
+                <CardTitle className="text-white text-center text-3xl font-bold tracking-tight">
                 Mestre das <span className="text-brand-primary"> Comandas</span>
                 </CardTitle>
             </CardHeader>
             <CardContent>
-                <form>
-                    <div>
-                        <Label htmlFor='name'>Nome</Label>
-                        <Input type='text' id='name' placeholder='Digite seu nome'></Input>
+                <form className="flex flex-col gap-4"> 
+                    <div className="flex flex-col gap-4">
+                        <Label htmlFor='name' className="text-white">
+                            Nome</Label>
+                        <Input
+                            type='text'
+                            id='name'
+                            placeholder='Digite seu nome'
+                            required
+                            minLength={3}
+                            className="text-white bg-app-card border-app-border"
+                        />
                     </div>
+
+                    <div className="flex flex-col gap-4">
+                        <Label htmlFor='name' className="text-white">
+                            Nome</Label>
+                        <Input
+                            type='text'
+                            id='email'
+                            placeholder='Digite seu email'
+                            required
+                            className="text-white bg-app-card border-app-border"
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-4">
+                        <Label htmlFor='name' className="text-white">
+                            Nome</Label>
+                        <Input
+                            type='password'
+                            id='password'
+                            placeholder='Digite seu password'
+                            required
+                            className="text-white bg-app-card border-app-border"
+                        />
+                    </div>
+
+                    <Button type="submit" className="w-full bg-brand-primary text-white hover:bg-brand-primary">
+                        Cadastrar
+                    </Button>
                 </form>
             </CardContent>
         </Card>
     </div>
+
+    
   )
 }
