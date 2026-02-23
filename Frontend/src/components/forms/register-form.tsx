bba055e8bbe5711c964d2dd98c23597dd19f4097
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-02-22 23:29:59 -0300

    feat: Imported and created new components @radix-ui:
    Card, CardTitle, CardHeader, CardContent, Label, Input,

--- Frontend/src/components/forms/register-form.tsx
+++ Frontend/src/components/forms/register-form.tsx
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+    CardDescription,
+    CardFooter,
+    CardContent
+} from "@/components/ui/card"
+import { Label } from '@radix-ui/react-label'
+import { Input } from '../ui/input'
 export function RegisterForm() {
   return (
     <div>
-        <h1>TESTE</h1>
+        <Card>
+            <CardHeader>
+                <CardTitle>
+                Mestre das <span className="text-brand-primary"> Comandas</span>
+                </CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form>
+                    <div>
+                        <Label htmlFor='name'>Nome</Label>
+                        <Input type='text' id='name' placeholder='Digite seu nome'></Input>
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
     </div>
   )
 }
