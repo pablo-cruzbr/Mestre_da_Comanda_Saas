c43f6135f61c5cc4b5d86f3fb355cade710ded05
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-24 16:41:59 -0300

    feat(dashboard): wrap layout in flex container to align sidebar and main content

--- Frontend/src/app/dashboard/layout.tsx
+++ Frontend/src/app/dashboard/layout.tsx
@@ -1,29 +1,30 @@
-//Escopo para criar layout dentro da pagina dashboard
 import { Header } from './components/Header/index' 
-import {Toaster} from 'sonner'
+import { Toaster } from 'sonner'
 import { OrderProvider } from '@/provider/order'
 import { Sidebar } from './components/dashboard/sidebar'
 
-export default function DashboardLayout({children}:
-    {children: React.ReactNode}
-){
-    return(
-        <>
-        {/* Sidebar Desktop */}
-        <Sidebar/>   
-        <OrderProvider>
-        {children}
-        </OrderProvider>
-        <Toaster 
-            position="bottom-right"
-            toastOptions={{
-                style:{
-                    backgroundColor:"#f1f1f1",
-                    color: "#131313",
-                    borderColor:"rgba(255,255,255,0.5)"
-                }
-            }}
+export default function DashboardLayout({children}: {children: React.ReactNode}) {
+    return (
+        <div className="flex h-screen bg-white">   
+            <Sidebar />   
+           <main className="flex-1 overflow-y-auto">
+                <OrderProvider>
+                    <div className="p-8"> 
+                        {children}
+                    </div>
+                </OrderProvider>
+            </main>
+
+            <Toaster 
+                position="bottom-right"
+                toastOptions={{
+                    style:{
+                        backgroundColor:"#f1f1f1",
+                        color: "#131313",
+                        borderColor:"rgba(255,255,255,0.5)"
+                    }
+                }}
             />
-        </>
+        </div>
     )
 }
