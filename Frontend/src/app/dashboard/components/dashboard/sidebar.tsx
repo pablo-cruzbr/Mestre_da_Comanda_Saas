c173f185cb42955a194a17ab3ac1d19890244e84
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-24 15:34:21 -0300

    feat(ui): implement main sidebar container and layout structure

--- Frontend/src/app/dashboard/components/dashboard/sidebar.tsx
+++ Frontend/src/app/dashboard/components/dashboard/sidebar.tsx
@@ -5,14 +5,29 @@ const menuItems = [
     {
         title: "Pedidos",
         href: "/dashboard",
-        icon:
+        icon: ShoppingCart
+    },
+    {
+        title: "Produtos",
+        href: "/dashboard/produtos",
+        icon: Package
+    },
+    {
+        title: "Categorias",
+        href: "/dashboard/category",
+        icon: Tags
     }
 ]
 
 export function Sidebar(){
     return(
-       <div>
-        <h1>SIDEBAR V2</h1>
-       </div>
+       <aside className="hidden lg:flex h-screen w-64 border-app-border bg-app-sidebar">
+
+        <div className="border-b border-app-border p-6">
+            <h2 className="text-x1 font-bold text-white">Mestre <span className="text-brand-primary"> das Comandas</span></h2>
+            <p className="text-sm text-gray-300 mt-1">Olá Fulano</p>
+        </div>
+
+       </aside>
     )
 }
