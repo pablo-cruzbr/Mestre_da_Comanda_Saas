46f887b59989a08108108d7a4355274dde530dfb
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-25 14:58:30 -0300

    feat: new menuList

--- Frontend/src/app/dashboard/components/dashboard/mobile-sidebar.tsx
+++ Frontend/src/app/dashboard/components/dashboard/mobile-sidebar.tsx
@@ -1,3 +1,23 @@
+
+import { ShoppingCart, Package, Tags, LogOut } from 'lucide-react'
+const menuItems = [
+    {
+        title: "Pedidos",
+        href: "/dashboard",
+        icon: ShoppingCart
+    },
+    {
+        title: "Produtos",
+        href: "/dashboard/produtos",
+        icon: Package
+    },
+    {
+        title: "Categorias",
+        href: "/dashboard/category",
+        icon: Tags
+    }
+]
+
 export function MobileSidebar(){
     return(
         <div>
