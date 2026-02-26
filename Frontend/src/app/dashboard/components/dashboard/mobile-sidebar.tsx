379c051b111c89908010c11098afe6c50081d873
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-02-26 15:25:58 -0300

    feat: import Button ui

--- Frontend/src/app/dashboard/components/dashboard/mobile-sidebar.tsx
+++ Frontend/src/app/dashboard/components/dashboard/mobile-sidebar.tsx
@@ -1,10 +1,11 @@
 "use client"
-import { ShoppingCart, Package, Tags, LogOut } from 'lucide-react'
+import { ShoppingCart, Package, Tags, LogOut, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname, useRouter} from "next/navigation"
 import { deleteCookie } from "cookies-next";
 import {toast} from 'sonner'
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { Button } from '@/components/ui/button';
 import { useState } from 'react';
 const menuItems = [
     {
@@ -37,7 +38,17 @@ export function MobileSidebar(){
        
     return(
         <div className='lg:hidden'>
-            <header className="sticky top-0 z-50 border-b border-app-border bg-app-card"></header>
+            <header className="sticky top-0 z-50 border-b border-app-border bg-app-card">
+                <div className="flex h-16 items-center justify-between px-4">
+                    <Sheet>
+                        <SheetTrigger>
+                            <Button>
+                                <Menu className="h-6 w-6"/>
+                            </Button>
+                        </SheetTrigger>
+                    </Sheet>
+                </div>
+            </header>
         </div>
     )
 }
