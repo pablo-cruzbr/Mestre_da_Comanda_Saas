31fb589118c77c94e09a584f2110eed2474e7a2d
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-02-28 13:23:06 -0300

    feat: add CategoryForm modal component and plus icon

---
+++ Frontend/src/app/dashboard/components/dashboard/category-form.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+import { useState } from "react"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Plus } from "lucide-react";
+import { Button } from "@/components/ui/button";
+
+export function CategoryForm() {
+    const [open, setOpen] = useState(false);
+    return(
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger>
+                <Button>
+                    <Plus className="h-5 w-5 mr-2"/>
+                </Button>
+            </DialogTrigger>
+        </Dialog>
+    )
+}
