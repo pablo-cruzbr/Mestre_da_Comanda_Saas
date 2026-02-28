a25bd3994c8a4d1176225eb78b1ded8045510085
Pablo Cruz <pablo.cruz3360@gmail.com>
2026-02-28 13:46:13 -0300

    update branch main styles

--- Frontend/src/app/dashboard/components/dashboard/category-form.tsx
+++ Frontend/src/app/dashboard/components/dashboard/category-form.tsx
@@ -12,23 +12,45 @@ import {
 } from "@/components/ui/dialog"
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Label } from "@radix-ui/react-label";
+import { Input } from "@/components/ui/input";
 
 export function CategoryForm() {
     const [open, setOpen] = useState(false);
-    return(
+
+    return (
         <Dialog open={open} onOpenChange={setOpen}>
-            <DialogTrigger>
-                <Button className="w-full sm:w-auto bg-brand-primary text-white border-none">
+            <DialogTrigger asChild>
+                <Button className="bg-red-600 hover:bg-red-700 text-white border-none">
                     <Plus className="h-5 w-5 mr-2"/>
                     Nova Categoria
                 </Button>
             </DialogTrigger>
 
-            <DialogContent>
+            <DialogContent className="bg-[#1d1d2e] border-gray-800 text-white">
                 <DialogHeader>
-                    <DialogHeader> Criar nova categoria</DialogHeader>
-                    <DialogDescription>Criando nova categoria...</DialogDescription>    
-                </DialogHeader>  
+                    <DialogTitle>Criar nova categoria</DialogTitle>
+                    <DialogDescription className="text-gray-400">
+                        Preencha o nome para adicionar ao seu cardápio.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <form className="space-y-4 mt-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="name">Nome da Categoria</Label>
+                        <Input
+                            id="name"
+                            name="name"
+                            required
+                            placeholder="Ex: Pizzas"
+                            className="bg-[#12121c] border-gray-700 text-white focus:border-red-500"
+                        />
+                    </div>
+                    
+                    <Button type="submit" className="w-full bg-brand-primary">
+                        Cadastrar
+                    </Button>
+                </form>
             </DialogContent>
         </Dialog>
     )
