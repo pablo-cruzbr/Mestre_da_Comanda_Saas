178505ca6adebcdf0019daa67f8ffebb9bf8a1ed
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-04-28 20:33:58 -0300

    styles: indentation

--- landing-page/src/components/Navbar.tsx
+++ landing-page/src/components/Navbar.tsx
@@ -5,8 +5,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Navbar = () => {
   return (
-    <header className="flex items-center justify-between py-4 px-5 lg:px-14 bg-white dark:bg-gray-900 transition-colors duration-300">
-        
+    <header className="flex items-center justify-between py-4 px-5 lg:px-14 bg-white dark:bg-gray-900 transition-colors duration-300">  
         <div className="flex items-center gap-1.5 text-2xl font-bold text-orange-500">
           <MdOutlineRestaurant/>
           <span>BiteHub</span>
