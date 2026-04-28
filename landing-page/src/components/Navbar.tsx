d30477c3e4bdf36290e19c298fadb1dc32b44023
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-04-28 12:29:02 -0300

    feat: Create Navbar scope (header, logo, links, button)
    Next step: styles

--- landing-page/src/components/Navbar.tsx
+++ landing-page/src/components/Navbar.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { FaShoppingCart } from 'react-icons/fa'
+import { MdOutlineRestaurant } from 'react-icons/md'
 
 const Navbar = () => {
   return (
-    <header>
-        <h1 className='bg-red-700'>NAVBAR</h1>
+    <header className="flex items-center justify-between py-4 px-5 lg:px-14">
+       <div className="flex items-center gap-1.5 text-2xl font-bold text-orange-500">
+        <MdOutlineRestaurant/>
+        <span>BiteHub</span>
+       </div>
+
+       <nav>
+        <a href="/">Home</a>
+        <a href="/">Restaurants</a>
+        <a href="/">Foods</a>
+        <a href="/">Service</a>
+       </nav>
+
+       <div>
+        <button>
+          <FaShoppingCart/>
+        </button>
+        <button>Register Now</button>
+       </div>
     </header>
   )
 }
