13f5d75cf92876598356e3ec8f0b53e09327eec4
Pablo Cruz <151473902+pablo-cruzbr@users.noreply.github.com>
2026-04-29 10:31:05 -0300

    feat(ui): implement GetApp section with dark mode support

--- landing-page/src/App.tsx
+++ landing-page/src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Process from './components/Process'
+import GetApp from './components/GetApp'
 const App = () => {
   return (
     <>
     <Navbar/>
     <Hero/>
     <Process/>
+    <GetApp/>
     </>
   )
 }
